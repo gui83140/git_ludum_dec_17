c14e50dccc1e2ed31b2bc547ce6d345ce5a59be1
Stonga <gaston.robert.auditeur@lecnam.net>
2017-12-03 20:10:24 +0100

    Menu

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour {
+
+    public GameObject jeu;
+
+	public void PlayGame()
+    {
+        jeu.SetActive(true);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
